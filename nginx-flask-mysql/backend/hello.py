17a3997b953dde8c503987e3759f1d0d61f57b59
Nathan Lively <ntlively@hotmail.com>
2021-09-16 16:10:19 -0400

    Working Connection to Redshift
    python psycopg2 connection working

--- nginx-flask-mysql/backend/hello.py
+++ nginx-flask-mysql/backend/hello.py
@@ -1,6 +1,10 @@
 import os
 from flask import Flask
 import mysql.connector
+import psycopg2
+from decouple import config
+
+
 
 
 class DBManager:
@@ -23,7 +27,7 @@ class DBManager:
         self.connection.commit()
     
     def query_titles(self):
-        self.cursor.execute('SELECT title FROM blog')
+        self.cursor.execute('SELECT firstname, lastname, total_quantity FROM   (SELECT buyerid, sum(qtysold) total_quantity FROM  sales GROUP BY buyerid ORDER BY total_quantity desc limit 10) Q, users WHERE Q.buyerid = userid ORDER BY Q.total_quantity desc;')
         rec = []
         for c in self.cursor:
             rec.append(c[0])
@@ -31,19 +35,22 @@ class DBManager:
 
 
 server = Flask(__name__)
-conn = None
+
 
 @server.route('/')
 def listBlog():
-    global conn
-    if not conn:
-        conn = DBManager(password_file='/run/secrets/db-password')
-        conn.populate_db()
-    rec = conn.query_titles()
-
+    userID = config('userID')
+    pw = config('pw')
+    
+    print(userID, pw)
+    conn = psycopg2.connect(database="dev", user = userID, password = pw, host = "redshift-cluster-1.ccs6bshicmph.us-east-1.redshift.amazonaws.com", port = "5439")
+    cursor = conn.cursor()
+    cursor.execute('SELECT firstname, lastname, total_quantity FROM   (SELECT buyerid, sum(qtysold) total_quantity FROM  sales GROUP BY buyerid ORDER BY total_quantity desc limit 10) Q, users WHERE Q.buyerid = userid ORDER BY Q.total_quantity desc;')
+    rec = []
+    for row in cursor.fetchall():
+        rec.append(row[0])
     response = ''
-    for c in rec:
-        response = response  + '<div>   Hello  ' + c + '</div>'
+    response = response  + '<div>   Hello  ' + "".join([str(i) for i in rec]) + '</div>'
     return response
 
 
